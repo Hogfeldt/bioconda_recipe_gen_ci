52b6a845f0b0b2271053d0e4b535b7a54b7015c1
Per Høgfeldt <hogfeldt38@gmail.com>
2019-05-23 16:15:42 +0200

    Initial cron wrapper commit

---
+++ cron-wrapper.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+###############################################
+# Cron wrapper for testing Bioconda-recipe-gen
+###############################################
+
+# TODO:
+# pull latest repository
+# setup environment
+# install software
+# extract all cases from test script 
+# run all cases and save the result of each case. 
+# send mail with result

bb0b96c2894f74e848b1c9b2c40559742b23989c
Per Høgfeldt <hogfeldt38@gmail.com>
2019-09-02 14:41:07 +0200

    relative paths fixed

--- cron-wrapper.sh
+++ cron-wrapper.sh
@@ -10,13 +10,21 @@ REPO=https://github.com/Hogfeldt/bioconda_recipe_gen.git
 NAME=$(basename "$REPO" .git)
 CONDA_DEPS="bioconda-utils docker-py"
 PACKAGES=("kallisto" "tn93" "htstream" "qfilt" "libdivsufsort" "clever-toolkit")
-TEST_DIR=~/bioconda_recipe_gen_cron_tests/$(date +%Y%m%d)
+BASE_PATH=/home/per/bioconda_recipe_gen_cron_tests
+TEST_DIR=$BASE_PATH/$(date +%Y%m%d)
 META_FILE=$TEST_DIR/test_meta.txt
+
+#####
+
+if [ -d $TEST_DIR ]; then
+    exit 1
+fi
+cd $BASE_PATH
 
 # TODO:
 # pull latest repository
 git clone $REPO
-cd "$NAME"
+cd $NAME
 # setup environment
 conda create --yes -n "$NAME" python=3.6 
 # the line below can only be done by root on server
@@ -32,7 +40,9 @@ python setup.py develop
 git clone https://github.com/bioconda/bioconda-recipes
 
 mkdir $TEST_DIR
+echo git_commit_sha: $(git rev-parse HEAD) >> $META_FILE
 echo start_time: $(date) >> $META_FILE
+# run all cases and save the result of each case. 
 for package in ${PACKAGES[*]}
 do
     ./travis/test.sh $package &> $TEST_DIR/$package.out
@@ -45,8 +55,6 @@ do
 done
 echo end_time: $(date) >> $META_FILE
 
-# extract all cases from test script 
-# run all cases and save the result of each case. 
 # send mail with result
 
 # Clean up environment
@@ -54,5 +62,5 @@ conda deactivate
 conda env remove --yes --name "$NAME"                                     
 
 # Remove bioconda_recipe_gen
-cd ..
+cd $BASE_PATH
 rm -rf "$NAME"

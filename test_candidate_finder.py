3b6f4b4c0bf6fdc999e17e478d2e63135d2fd087
Per Høgfeldt <hogfeldt38@gmail.com>
2019-09-04 09:31:48 +0200

    First function should be done
    The function for harvesting urls and names from bioconda recipes should
    be done

--- test_candidate_finder.py
+++ test_candidate_finder.py
@@ -1,4 +1,7 @@
 import argparse
+import os
+
+from meta_yaml_parser import meta_yaml_to_dict
 
 
 def write_candidates_to_file(candidates, filepath):
@@ -25,11 +28,39 @@ def get_packages_containing_cmakelist_in_root(candidates):
     return []
 
 
-def get_all_source_urls(filepath):
+def get_all_source_urls(recipes_path):
     """ Traverse bioconda/recipes and extract the tupple (Name, Source_Url) 
         from all packages.
         """
-    return []
+    packages = []
+    for subdir, dirs, files in os.walk(recipes_path):
+        for dir in dirs:
+            meta_yaml_path = "%s/%s/meta.yaml" % (recipes_path, dir)
+            if not os.path.isfile(meta_yaml_path):
+                continue
+            name = ""
+            url = ""
+            meta_yaml_dict = meta_yaml_to_dict(meta_yaml_path)
+            if meta_yaml_dict != None:
+                # extract url
+                if (
+                    isinstance(meta_yaml_dict["source"], list)
+                    and "url" in meta_yaml_dict["source"][0]
+                ):
+                    url = meta_yaml_dict["source"][0]["url"]
+                elif "url" in meta_yaml_dict["source"]:
+                    url = meta_yaml_dict["source"]["url"]
+                # extract name
+                if (
+                    isinstance(meta_yaml_dict["package"], list)
+                    and "name" in meta_yaml_dict["package"][0]
+                ):
+                    name = meta_yaml_dict["package"][0]["name"]
+                elif "name" in meta_yaml_dict["package"]:
+                    name = meta_yaml_dict["package"]["name"]
+                # save in packages
+                packages += (name, url)
+    return packages
 
 
 def main():
@@ -37,13 +68,15 @@ def main():
     parser = argparse.ArgumentParser(
         description="Script for finding a set of bioconda packages, which are build with CMake and where the bioconda recipe is able to build itself"
     )
-    parser.add_argument("recipes_path", help="Path to bioconda/recipe folder")
+    parser.add_argument("recipes_path", help="Path to bioconda-recipes/recipes folder")
     args = parser.parse_args()
 
     # Run workflow
     recipes_path = args.recipes_path
-    source_urls = get_all_source_urls(recipes_path)
-    candidates = get_packages_containing_cmakelist_in_root(source_urls)
+    packages = get_all_source_urls(recipes_path)
+    for package in packages:
+        print(package)
+    candidates = get_packages_containing_cmakelist_in_root(packages)
     candidates = filter_candidates(candidates)
     write_candidates_to_file(candidates, "file_path")
     pass

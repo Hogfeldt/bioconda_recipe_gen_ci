a4810c2ed6548e6998d19faec60dff29fc2de236
Per Høgfeldt <hogfeldt38@gmail.com>
2019-09-13 16:51:38 +0200

    The git-differ needs to rewritten to use the PackageDB

--- git_differ.py
+++ git_differ.py
@@ -1,21 +1,26 @@
+import argparse
 from git import Repo
 from os.path import isfile
+import os
 
 from .utils import get_brg_ci_homedir_path
 
 CI_HOMEDIR = get_brg_ci_homedir_path()
-COMMIT_FILE = CI_HOMEDIR + 'latest_commit.data'
+COMMIT_FILE = CI_HOMEDIR + "latest_commit.data"
+
 
 def get_latest_commit():
-    with open(COMMIT_FILE, 'r') as fp:
+    with open(COMMIT_FILE, "r") as fp:
         sha = fp.readline()
-        if sha.endswith('/n'):
-            sha = sha[:-len('/n')]
+        if sha.endswith("/n"):
+            sha = sha[: -len("/n")]
         return sha
 
-def get_all_source_urls(recipes_path):
-    """ Traverse bioconda/recipes and extract the tupple (Name, Source_Url)
-        from all packages.
+
+def retrive_packages_changed(recipes_path):
+    """ Traverse bioconda-recipes and extract all packages which has changed.
+
+        Assumptions: We can safely ignore Bioconductor and R packages
         """
     print("Fetching packages Name and Source...")
     packages = []
@@ -23,11 +28,11 @@ def get_all_source_urls(recipes_path):
     num_of_skipped = 0
     if isfile(COMMIT_FILE):
         latest_commit = get_latest_commit()
-        head_commit = git.Repo(recipes_path+'/..').head.commit
+        head_commit = git.Repo(recipes_path + "/..").head.commit
         diff = hcommit.diff(latest_commit)
-        dirs = [ item.a_path.split('/')[1] for item in diff] 
+        dirs = [item.a_path.split("/")[1] for item in diff]
     else:
-    _, dirs, _ = next(os.walk(recipes_path))
+        _, dirs, _ = next(os.walk(recipes_path))
     for dir in dirs:
         num_of_packages += 1
         # Skip Bioconductor or R packages
@@ -42,8 +47,8 @@ def get_all_source_urls(recipes_path):
             name = current_recipe.name
             try:
                 url = current_recipe.get("source/url")
-                if(type(url) is not str):
-                    url=url[0]
+                if type(url) is not str:
+                    url = url[0]
             except:
                 print("%s raised an Error" % name)
                 continue
@@ -51,4 +56,17 @@ def get_all_source_urls(recipes_path):
         except:
             print("%s raised an Error" % dir)
     print("%d out of %d packages where skipped" % (num_of_skipped, num_of_packages))
-    return packages
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="The git-differ module will take the path to the Bioconda-recipe recipes and create a list of packages changed from the commit-sha saved in '~/.brg-ci/latest_commit.data' and the HEAD-commit. The packages will be stored in '~/.brg-ci/packages_changed.yaml"
+    )
+    parser.add_argument(
+        "recipes_path", help="The path to the bioconda-recipe/recipes directory"
+    )
+    get_all_source_urls(args.recipes_path)
+
+
+if __name == "__main__":
+    main()
